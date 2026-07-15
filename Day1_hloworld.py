d036c477778a1d08f25d098dfe2efce7b830443b
Bhavya G <bhavyamadyalkar4@gmail.com>
2026-07-15 18:09:29 +0530

    Add files via upload

--- Day1_hloworld.py
+++ Day1_hloworld.py
@@ -1,2 +1,7 @@
-my_string= "Hello, World!"
-print("Hello, World!");
+inputString = input() # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+print('Hello, World.')
+print(inputString)
+
+# Write the second line of output
